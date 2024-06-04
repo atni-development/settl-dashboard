b618eee74d0547d7d94a3be6078788eb95aa46b5
Ivan Casillas Tovar <ivan.cstv@gmail.com>
2024-06-04 12:05:42 -0600

    udpated time

--- components/depositMoney/StepThree.jsx
+++ components/depositMoney/StepThree.jsx
@@ -67,7 +67,7 @@ const StepThree = () => {
         
         var diff = currentDate - sessionDate;
         var diffMinutes = Math.round(diff / 60000);
-        if(diffMinutes > 1){
+        if(diffMinutes > 10){
           localStorage.removeItem(userId+'session_date');
           localStorage.removeItem(userId+'current_card');
           localStorage.removeItem(userId+'amountToPay');
