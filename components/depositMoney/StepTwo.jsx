7ae76bd46de9099953a5bc0081860a171950b105
Ivan Casillas Tovar <ivan.cstv@gmail.com>
2024-07-04 12:34:51 -0600

    changed again scripts

--- components/depositMoney/StepTwo.jsx
+++ components/depositMoney/StepTwo.jsx
@@ -90,9 +90,8 @@ const StepTwo = () => {
   return (
     <section className="dashboard-section body-collapse pay step crypto deposit-money">
           <Head>
+          <script type="text/javascript" async="false" defer="false" src="https://resources.openpay.mx/lib/openpay-js/1.2.38/openpay.v1.min.js"></script>
           <script type="text/javascript" async="false" defer="false" src="https://resources.openpay.mx/lib/openpay-data-js/1.2.38/openpay-data.v1.min.js"></script>
-
-        <script type="text/javascript" async="false" defer="false" src="https://resources.openpay.mx/lib/openpay-js/1.2.38/openpay.v1.min.js"></script>
       </Head>
       <div className="overlay pt-120">
         <div className="container-fruid">
