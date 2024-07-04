c487fa920dde2170adaf0a5d58e5749a355915b9
Ivan Casillas Tovar <ivan.cstv@gmail.com>
2024-07-04 12:25:09 -0600

    changed scripts order

--- components/depositMoney/StepThree.jsx
+++ components/depositMoney/StepThree.jsx
@@ -328,8 +328,9 @@ const StepThree = () => {
   return (
     <section className="dashboard-section body-collapse pay step step-3 crypto deposit-money">
       <Head>
+      <script type="text/javascript" async="false" defer="false" src="https://resources.openpay.mx/lib/openpay-data-js/1.2.38/openpay-data.v1.min.js"></script>
+
         <script type="text/javascript" async="false" defer="false" src="https://resources.openpay.mx/lib/openpay-js/1.2.38/openpay.v1.min.js"></script>
-        {/* <script type="text/javascript" async="false" defer="false" src="https://resources.openpay.mx/lib/openpay-data-js/1.2.38/openpay-data.v1.min.js"></script> */}
       </Head>
       <div className="overlay pt-120">
         <div className="container-fruid">
