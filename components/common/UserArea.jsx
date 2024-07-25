9f733c2fc4ba680889a0f91ea69cc7dee866a124
Ivan Casillas Tovar <ivan.cstv@gmail.com>
2024-07-25 17:04:38 -0600

    avatar fix

--- components/common/UserArea.jsx
+++ components/common/UserArea.jsx
@@ -8,12 +8,12 @@ import { useState, useEffect } from 'react';
 
 import { FaCog, FaSignOutAlt, FaSortDown } from "react-icons/fa";
 import avatar_2 from "/public/images/avatar-2.png";
-import avatar from "/public/images/avatar.png";
+
 import { Button } from "reactstrap";
 
 const UserArea = () => {
   const { userActiveHandler, userActive } = useContext(SettlContext);
-
+  const avatar  = "https://firebasestorage.googleapis.com/v0/b/settl-project.appspot.com/o/owner-profile.png?alt=media&token=49f92549-3821-4964-a3a4-bc88312836d2"
   const onLogOut = event => {
     console.log("logout");
     const auth = getAuth();
